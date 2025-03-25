e5302a7bfcea8ed714556ce9d660897e1d2429d0
AttilaBehan <57575590+AttilaBehan@users.noreply.github.com>
2025-03-25 09:24:30 +0100

    Update Feature_Extraction.py
    Added function to extract features to CSV files

--- Feature_Extraction.py
+++ Feature_Extraction.py
@@ -1,7 +1,11 @@
 import numpy as np
 from scipy import stats
 import pandas as pd
+import math
+import os
+import warnings
 
+warnings.simplefilter(action='ignore')
 
 def CSV_to_Array(file):
     """
@@ -19,6 +23,7 @@ def CSV_to_Array(file):
     sample_df = pd.read_csv(file)  # Converts csv into dataframe
 
     # Convert respective dataframe colum to a numpy array
+    sample_time=sample_df["Time"].to_numpy()
     sample_amplitude = sample_df["Amplitude"].to_numpy()
     sample_risetime = sample_df["Rise-Time"].to_numpy()
     sample_energy = sample_df["Energy"].to_numpy()
@@ -26,7 +31,7 @@ def CSV_to_Array(file):
     sample_duration = sample_df["Duration"].to_numpy()
     sample_rms = sample_df["RMS"].to_numpy()
 
-    return sample_amplitude, sample_risetime, sample_energy, sample_counts, sample_duration, sample_rms
+    return sample_time, sample_amplitude, sample_risetime, sample_energy, sample_counts, sample_duration, sample_rms
 
 def Time_Domain_Features(data):
     """ Code copied/adapted from last year. """
@@ -96,3 +101,44 @@ def Time_Domain_Features(data):
     T_features[18] = np.median(X)
 
     return T_features
+
+def time_windows_extract(cycle_length, dir):
+
+    for root, dirs, samples in os.walk(dir):
+        for sample in samples:
+            file=os.path.join(root, sample)
+            
+            df = pd.read_csv(file)
+            print(file)
+            end_val=math.ceil(df["Time"].iloc[-1] / cycle_length) * cycle_length
+            cycles=np.arange(cycle_length, end_val+1, cycle_length)
+            prev_index=-1
+
+            #define the time domain features dataframe
+            base_time_features=['Mean','Standard_deviation','Root_amplitude','Root_mean_squared','Root_standard_squared','Peak','Skewness','Kurtosis','Crest_factor','Clearance_factor','Shape_factor','Impulse_factor','Max_Min_difference','Central_moment_3rd_order','Central_moment_4th_order','Central_moment_5th_order','Central_moment_6th_order','FM4','Median']
+            time_features=[]
+            for column in df.iloc[:, 1:]:
+                time_features.append([str(column)+"_"+i for i in base_time_features])
+            time_features=np.concatenate(time_features)
+            features_df=pd.DataFrame(columns=time_features)
+
+            output_dir = r"C:\Users\attil\OneDrive\TU_Delft\Project_SHM\Time_Domain_Features_500_500_CSV"
+            os.makedirs(output_dir, exist_ok=True)
+
+            for cycle in cycles:
+                current_row=[]
+                index=df["Time"][df["Time"]<=cycle].index[-1] 
+                for column in df.iloc[:, 1:]:
+                    try:
+                        current_row.append(Time_Domain_Features(df[column][prev_index+1:index+1].to_numpy().flatten()))
+                    except:
+                        current_row.append([np.nan]*19)
+                current_row=np.concatenate(current_row)
+                new_row=pd.DataFrame([current_row], columns=time_features)
+                features_df = pd.concat([features_df, new_row], ignore_index=True)
+                prev_index=index
+
+            features_df.insert(0, "Time (cycle)", cycles)
+            features_df=features_df.dropna()
+            csv_filename = os.path.join(output_dir, f"{sample[:-4]}.csv")
+            features_df.to_csv(csv_filename, index=False)    
