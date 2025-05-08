87fef33dbec8d6bac8d5680312dacecc091080d7
Philip Benschop <p.h.a.benschop@student.tudelft.nl>
2025-05-08 16:49:40 +0200

    DCEC
    This is the DCEC code working for the testing dataset, however it's accuracy is not great yet

--- DCEC/ConvAE.py
+++ DCEC/ConvAE.py
@@ -1,32 +1,47 @@
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense, Flatten, Reshape
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.utils import plot_model
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
 import numpy as np
 
 
-def CAE(input_shape=(28, 28, 1), filters=[32, 64, 128, 10]):
-    model = Sequential()
-    if input_shape[0] % 8 == 0:
-        pad3 = 'same'
-    else:
-        pad3 = 'valid'
-    model.add(Conv2D(filters[0], 5, strides=2, padding='same', activation='relu', name='conv1', input_shape=input_shape))
+# def CAE(input_shape=(28, 28, 1), filters=[32, 64, 128, 10]):
+#     model = Sequential()
+#     if input_shape[0] % 8 == 0:
+#         pad3 = 'same'
+#     else:
+#         pad3 = 'valid'
+#     model.add(Conv2D(filters[0], 5, strides=2, padding='same', activation='relu', name='conv1', input_shape=input_shape))
+#
+#     model.add(Conv2D(filters[1], 5, strides=2, padding='same', activation='relu', name='conv2'))
+#
+#     model.add(Conv2D(filters[2], 3, strides=2, padding=pad3, activation='relu', name='conv3'))
+#
+#     model.add(Flatten())
+#     model.add(Dense(units=filters[3], name='embedding'))
+#     model.add(Dense(units=filters[2]*int(input_shape[0]/8)*int(input_shape[0]/8), activation='relu'))
+#
+#     model.add(Reshape((int(input_shape[0]/8), int(input_shape[0]/8), filters[2])))
+#     model.add(Conv2DTranspose(filters[1], 3, strides=2, padding=pad3, activation='relu', name='deconv3'))
+#
+#     model.add(Conv2DTranspose(filters[0], 5, strides=2, padding='same', activation='relu', name='deconv2'))
+#
+#     model.add(Conv2DTranspose(input_shape[2], 5, strides=2, padding='same', name='deconv1'))
+#     model.summary()
+#     return model
 
-    model.add(Conv2D(filters[1], 5, strides=2, padding='same', activation='relu', name='conv2'))
+def dense_CAE(input_shape=(6,), hidden_units=[64, 32, 16]):
+    input_layer = Input(shape=input_shape, name='input')
+    x = Dense(hidden_units[0], activation='relu')(input_layer)
+    x = Dense(hidden_units[1], activation='relu')(x)
+    encoded = Dense(hidden_units[2], activation='relu', name='embedding')(x)
 
-    model.add(Conv2D(filters[2], 3, strides=2, padding=pad3, activation='relu', name='conv3'))
+    x = Dense(hidden_units[1], activation='relu')(encoded)
+    x = Dense(hidden_units[0], activation='relu')(x)
+    decoded = Dense(input_shape[0], name='decoder')(x)
 
-    model.add(Flatten())
-    model.add(Dense(units=filters[3], name='embedding'))
-    model.add(Dense(units=filters[2]*int(input_shape[0]/8)*int(input_shape[0]/8), activation='relu'))
-
-    model.add(Reshape((int(input_shape[0]/8), int(input_shape[0]/8), filters[2])))
-    model.add(Conv2DTranspose(filters[1], 3, strides=2, padding=pad3, activation='relu', name='deconv3'))
-
-    model.add(Conv2DTranspose(filters[0], 5, strides=2, padding='same', activation='relu', name='deconv2'))
-
-    model.add(Conv2DTranspose(input_shape[2], 5, strides=2, padding='same', name='deconv1'))
-    model.summary()
+    model = Model(inputs=input_layer, outputs=decoded)
     return model
 
 if __name__ == "__main__":
@@ -35,8 +50,8 @@ if __name__ == "__main__":
     # setting the hyper parameters
     import argparse
     parser = argparse.ArgumentParser(description='train')
-    parser.add_argument('--dataset', default='usps', choices=['mnist', 'usps'])
-    parser.add_argument('--n_clusters', default=10, type=int)
+    parser.add_argument('--dataset', default='aedata', choices=['aedata', 'mnist', 'usps'])
+    parser.add_argument('--n_clusters', default=5, type=int)
     parser.add_argument('--batch_size', default=256, type=int)
     parser.add_argument('--epochs', default=200, type=int)
     parser.add_argument('--save_dir', default='results/temp', type=str)
@@ -48,14 +63,16 @@ if __name__ == "__main__":
         os.makedirs(args.save_dir)
 
     # load dataset
-    from datasets import load_mnist, load_usps
+    from datasets import load_mnist, load_usps, load_aedata
+    if args.dataset == 'aedata':
+        x, y = load_aedata()
     if args.dataset == 'mnist':
         x, y = load_mnist()
     elif args.dataset == 'usps':
         x, y = load_usps('data/usps')
 
     # define the model
-    model = CAE(input_shape=x.shape[1:], filters=[32, 64, 128, 10])
+    model = dense_CAE(input_shape=x.shape[1:], hidden_units=[64, 32, 16])
     plot_model(model, to_file=args.save_dir + '/%s-pretrain-model.png' % args.dataset, show_shapes=True)
     model.summary()
 
